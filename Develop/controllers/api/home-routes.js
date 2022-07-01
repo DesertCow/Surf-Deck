af42345978128dbe14f9458832e1c06c3232bcd5
Clayton S <desertcow07@gmail.com>
2022-07-01 11:29:04 -0700

    super broken

---
+++ Develop/controllers/api/home-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const Surfboard = require('../../models/surfboard')
+
+
+// the '/api/surfboards' endpoint
+
+
+//View all information from surfboard table
+router.get('/', (req, res) => {
+  Surfboard.findAll()
+
+  res.render('home', {
+  })
+
+
+  // .then(surfboardData => res.json(surfboardData))
+  // .catch(err => {
+  //   console.log(err);
+  //   res.status(500).json(err);
+  // });
+})
+
+
+module.exports = router
