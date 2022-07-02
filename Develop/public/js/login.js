b8ddfa0ac628988c894c6300ef829537dd885863
Clayton S <desertcow07@gmail.com>
2022-07-02 08:54:16 -0700

    Enabled front end Login/Signup Pages

---
+++ Develop/public/js/login.js
@@ -0,0 +1,56 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#loginbox-Email').value.trim();
+  const password = document.querySelector('#loginbox-Password').value.trim();
+
+  console.log("Email = " + email + " || " + "Password = " + password);
+  window.alert("\n🏄 🏄 🏄 🏄 🏄 🏄 🏄 🏄 🏄 🏄\nWelcome Back! \n 📧 =" + email + "\n" + "🗝️ = " + password + "\n\n Login Sugcessful! \n🏄 🏄 🏄 🏄 🏄 🏄 🏄 🏄 🏄 🏄\n");
+
+  if (email && password) {
+    // const response = await fetch('/api/users/login', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ email, password }),
+    //   headers: { 'Content-Type': 'application/json' },
+    // });
+
+    // if (response.ok) {
+    if (1) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in.');
+    }
+  }
+};
+
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#FullName').value.trim();
+  const email = document.querySelector('#userEmail').value.trim();
+  const password = document.querySelector('#userPassword').value.trim();
+  const doublePassword = document.querySelector('#doubleUserPassword').value.trim();
+
+  window.alert("Name:" + username + "\n Email:" + email + "\n Password:" + password + "\n Password 2:" + doublePassword)
+
+  if (username && email && password && doublePassword) {
+
+    // const response = await fetch('/api/users', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ username, email, password }),
+    //   headers: { 'Content-Type': 'application/json' },
+    // });
+
+    if (1) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+//!========================= EOF =========================
