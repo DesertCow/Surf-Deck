818cab19f92e8eecaf45f4c5412eb0daa31eceeb
Clayton S <desertcow07@gmail.com>
2022-07-06 21:06:11 -0700

    Typo fix...

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
- const express = require('express');
+const express = require('express');
 const session = require('express-session')
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
